a507e3583fa77ae3745f4228d2ab2bd1d9c4050a
Gonthini Bhaskar <49478324+bhaskargonthini@users.noreply.github.com>
2022-07-01 13:03:03 +0530

    Add files via upload

---
+++ rightMostDifferenceBit.cpp
@@ -0,0 +1,44 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find the first position with different bits.
+    int posOfRightMostDiffBit(int m, int n)
+    {
+        // Your code here
+        m = m^n;
+        int count = 1;
+        while(m>0){
+            if(m & 1 == 1){
+                return count;
+            }
+            count++;
+            m = m>>1;
+        }
+        return -1;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver Code
+int main()
+{   
+    int t;
+    cin>>t; //input number of testcases
+    while(t--)
+    {
+         int m,n;
+         cin>>m>>n; //input m and n
+         Solution ob;
+         cout << ob.posOfRightMostDiffBit(m, n)<<endl;
+    }
+    return 0;     
+}   // } Driver Code Ends
